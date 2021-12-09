f07404713a7745e5b5dc12f816cb63747bc81766
Will Lindstrom <wlindstrom55@gmail.com>
2021-12-08 19:51:10 -0600

    adding completed version of hamming exercise

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -2,22 +2,33 @@ public class Hamming {
 	public int hammingD;
 	
     public Hamming(String leftStrand, String rightStrand) {
-    		//first we check if the two strings have the same length, if not, exception is thrown
-    	if(leftStrand.length() != rightStrand.length()) {
-    		throw new IllegalArgumentException();
+    /*
+     * first we do our checks to make sure neither String parameter is empty, and 
+     * that they are of equal length.
+     */
+    	if(leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+    		throw new IllegalArgumentException("left strand must not be empty.");
     	}
+    	if(!leftStrand.isEmpty() && rightStrand.isEmpty()) {
+    		throw new IllegalArgumentException("right strand must not be empty.");
+    	}
+    	if(leftStrand.length() != rightStrand.length()) {
+    		throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+    	}
+    	if(leftStrand.length() == rightStrand.length()) {
     		//if both lengths are the same, we continue further comparison
-    		//we iterate over both strings comparing chars. If both are the same, nothing happens. If one is different, we add it to our counter.
-    	for(int i = 0; i <leftStrand.length(); i++) {
+    		//we iterate over both strings comparing chars. If both are the same, 
+    		//nothing happens. If one is different, we add it to our counter.
+    	for(int i = 0; i < leftStrand.length(); i++) {
     		if(leftStrand.charAt(i) != rightStrand.charAt(i)) {
     			hammingD += 1;
     		}
     	}
+    	}
     
     }
 
-    public int getHammingDistance() {
+    public int getHammingDistance() { //this method returns the instance variable of our Hamming Distance.
         return this.hammingD = hammingD;
-    	//throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 }
